5ce1dfd7bf8e0ea0da0a1db8447bbffb959cff5b
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2015-03-19 16:06:45 +0000

    C3 - default config & tidy up

--- assets/js/client/c3/c3-transform-grouped.js
+++ assets/js/client/c3/c3-transform-grouped.js
@@ -1,9 +1,5 @@
 define([], function() {
   return function (module) {
-    var x = [],
-      y = [],
-      xKey = (typeof module.axes.x.key === 'string') ? module.axes.x.key : module.axes.x.key[0];
-
     return {
       data: {
         x: 'Date',
--- assets/js/client/c3/c3-transforms.js
+++ assets/js/client/c3/c3-transforms.js
@@ -1,11 +1,18 @@
-define(['c3TransformDefault', 'c3TransformGrouped'], function(c3TransformDefault, c3TransformGrouped) {
+define(['c3TransformDefault', 'c3TransformGrouped', 'lodash'], function(c3TransformDefault, c3TransformGrouped, _) {
   return function (module) {
+    var config;
     switch (module.moduleConfig['module-type']) {
       case 'grouped_timeseries':
-        return c3TransformGrouped(module);
+        config = c3TransformGrouped(module);
+        break;
       default:
-        return c3TransformDefault(module);
+        config = c3TransformDefault(module);
     }
+    return _.extend({
+      point: {
+        show: false
+      }
+    }, config);
   };
 
 });
